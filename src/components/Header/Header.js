ed256e24fb1c62a27fa832ec7896ede6e855e77a
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-04-04 15:46:38 -0400

    Organizing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return(
             <div id="home">
-                <h1 class="header-h1">Grisha Kramer Profile</h1>
+                <h1 className="header-h1">Grisha Kramer Profile</h1>
             </div>
         )
     }
