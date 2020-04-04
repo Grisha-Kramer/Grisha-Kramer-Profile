6b75dbb0b9baa8bd288df42ae76c3d4c7849d213
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-04-04 14:04:22 -0400

    Structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import "./style.css"
+
+class Header extends Component {
+    render() {
+        return(
+            <div id="home">
+                <h1 class="header-h1">Grisha Kramer Profile</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
