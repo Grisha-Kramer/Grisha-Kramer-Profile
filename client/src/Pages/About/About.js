27551cd2461d684956d14e135d79122f284b3eb0
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-04-19 18:28:44 -0400

    adding logos

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -15,54 +15,49 @@ function About() {
             width="100px"
           />
           <h1>Grisha Kramer</h1>
-          <p> I am creative full stack Developer based in Richmond, VA</p>
+          <p>Full stack Developer based in Richmond with extensive experience working in startups</p>
           <h1>Hobbies and fun</h1>
           <p>Riding and repairing bicycles</p>
           <p>Guitar, Keys, Drums, Accordion, Cello, Beatbox</p>
-          <p>Intense culinary activities, sourdough, pizza tossing etc</p>
+          <p>Intense culinary activities, sourdough, pizza tossing, cheesemaking etc etc</p>
+        </div>
+
+        <div className="col-xs-4 col-lg-4 text-center">
+          <img
+            src="../icons/Lightbulb-01.png"
+            styles="width:50%"
+            alt="Gear"
+            classname="icon"
+            width="100px"
+          ></img>
+          <h1>Front End</h1>
+          <p>HTML</p>
+          <p>Javascript</p>
+          <p>jQuery</p>
+          <p>#CSS</p>
+          <p>Bootstrap</p>
+          <p>AJAX</p>
+          <p>JSON</p>
+          <p>React</p>
         </div>
 
         <div className="col-xs-4 col-lg-4 text-center">
           <img
             src="../icons/Gear-01.png"
             styles="width:50%"
-            alt="Gear"
-            classname="icon"
-            width="100px"
-          ></img>
-          <h1>Front | Back </h1>
-          <p>HTML |APIs</p>
-          <p>Javascript | Express</p>
-          <p>jQuery | Passport</p>
-          <p>#CSS | Node.js</p>
-          <p>Bootstrap | mySQL</p>
-          <p>AJAX | Mongo</p>
-          <p>JSON | Mongoose</p>
-          <p>React | </p>
-        </div>
-                   
-
-        <div className="col-xs-4 col-lg-4 text-center">
-          <img
-            src="../icons/Suitcase-01.png"
-            styles="width:50%"
             alt="Suitcase"
             classname="icon"
             width="100px"
           ></img>
-          <h1>Professional Skills</h1>
-          <p>Google Ads</p>
-          <p>Google Tag Manager</p>
-          <p>Google Optimize</p>
-          <p>Google Analytics</p>
-          <p>Facebook Pixel</p>
-          <p>Facebook Ad Manager</p>
-          <p>SEMRush</p>
-          <p>Vlookup("Advanced Excel", "Skills",2,TRUE)</p>
-          <p>Tableau</p>
-          <p>Hootsuite</p>
-          <p>Mailchimp</p>
-          <p>Recruiting, hiring and training</p>
+          <h1>Backend</h1>
+          <p>APIs</p>
+          <p>Express</p>
+          <p> Passport</p>
+          <p>Node.js</p>
+          <p>mySQL</p>
+          <p>noSQL</p>
+          <p>Mongo</p>
+          <p>Mongoose</p>
         </div>
       </div>
     </>
--- client/src/Pages/Contact/Contact.js
+++ client/src/Pages/Contact/Contact.js
@@ -4,33 +4,53 @@ import "./style.css";
 function Contact() {
   return (
     <div className="container" styles="font-family: Montserrat">
-      <div className="row">
-        <div className="col-xs-4 col-lg-4 text-center">
-          <img
-            src="../icons/Pen-01.png"
-            styles="width:50%"
-            alt="Pen"
-            className="icon"
-            width="100px"
-          ></img>
-          <h1>Contact</h1>
-          <p>Email: grisharkramer@gmail.com </p>
-          <p>
-            <a href="https://www.linkedin.com/in/grisha-kramer-65b8777b/">
-              Linkedin
-            </a>
-          </p>
-          <p>
-            <a href="https://www.linkedin.com/in/grisha-kramer-65b8777b/">
-              Github
-            </a>
-          </p>
-          <p>
-            <a href="https://drive.google.com/open?id=1R7CZXNTuaiB65xl1VkMPnI4Xk1lpkr5-">
-              Resume
-            </a>
-          </p>
-        </div>
+      <div className="col-xs-4 col-lg-4 text-center">
+        <img
+          src="../icons/Grisha.jpg"
+          styles="width:50%"
+          alt="Pen"
+          className="icon"
+          width="100px"
+        ></img>
+        <h1>Contact</h1>
+        <p>Email: grisharkramer@gmail.com </p>
+        <p>
+          <a href="https://www.linkedin.com/in/grisha-kramer-65b8777b/">
+            Linkedin
+          </a>
+        </p>
+        <p>
+          <a href="https://www.linkedin.com/in/grisha-kramer-65b8777b/">
+            Github
+          </a>
+        </p>
+        <p>
+          <a href="https://drive.google.com/open?id=1R7CZXNTuaiB65xl1VkMPnI4Xk1lpkr5-">
+            Resume
+          </a>
+        </p>
+      </div>
+      <div className="col-xs-4 col-lg-4 text-center">
+        <img
+          src="../icons/Suitcase-01.png"
+          styles="width:50%"
+          alt="Suitcase"
+          classname="icon"
+          width="100px"
+        ></img>
+        <h1>Professional Skills</h1>
+        <p>Google Ads</p>
+        <p>Google Tag Manager</p>
+        <p>Google Optimize</p>
+        <p>Google Analytics</p>
+        <p>Facebook Pixel</p>
+        <p>Facebook Ad Manager</p>
+        <p>SEMRush</p>
+        <p>Vlookup("Advanced Excel", "Skills",2,TRUE)</p>
+        <p>Tableau</p>
+        <p>Hootsuite</p>
+        <p>Mailchimp</p>
+        <p>Recruiting, hiring and training</p>
       </div>
     </div>
   );
